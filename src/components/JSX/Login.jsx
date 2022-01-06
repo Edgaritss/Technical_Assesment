3b053da923f0ff72c770466effb15eb4eb093e9c
Edgar Rojas <garos19480@gmail.com>
2022-01-06 16:46:15 -0600

    Firebase Login Functionality and Firebase Configurations JS

--- src/components/JSX/Login.jsx
+++ src/components/JSX/Login.jsx
@@ -1,19 +1,31 @@
 
-import {Flex,Heading,Input,Button,useColorMode, useColorModeValue} from '@chakra-ui/react'
-import React,{useState} from 'React';
-import {auth} from '../fireconfig';
+import {Flex,Heading,Input,Button,useColorMode,FormControl ,useColorModeValue} from '@chakra-ui/react';
+import {useState} from "react";
+import { fire } from '../fireconfig';
 
 const LoginPage =() =>{
     const {toggleColorMode} = useColorMode()
     const formBackground = useColorModeValue("gray.100", "gray.700")
+    const [email,setEmail] = useState('')
+    const [pass,setPass] = useState('')
+
+    const registerUser =(e) =>{
+        console.log("email",email,"password",pass)
+        e.preventDefault()
+        fire.auth().createUserWithEmailAndPassword(email,pass)
+        .then((res) => alert('Succesfully created'))
+    }
     return(
     <Flex height="100vh" alignItems="center" justifyContent="center">
-        <Flex direction="column" background={formBackground} p={12} rounded={6}>
+        <Flex direction="column" background={formBackground} p={8} rounded={6}>
             <Heading textAlign="center" mb={6}>Log in</Heading>
-            <Input placeholder="EDGAR ROJAS" variant="filled" mb={3} type="email"/>
-            <Input placeholder="******" variant="filled" mb={6} type="password"/>
-            <Button colorScheme="teal" >Log in</Button>
-            <Button onClick={toggleColorMode} >Dark Mode</Button>
+            <Input onChange={(e) => {setEmail(e.target.value)}} placeholder="EDGAR ROJAS" variant="filled" mb={3} type="email"/>
+
+            <Input onChange={(e) => {setPass(e.target.value)}}  placeholder="******" variant="filled" mb={6} type="password"/>
+
+            <Button mt={2} onClick={registerUser} colorScheme="teal" >Log in</Button>
+            <Button mt={2} onClick={registerUser} colorScheme="blue" >Google</Button>
+            <Button mt={2} onClick={toggleColorMode} >Dark Mode</Button>
 
         </Flex>
     </Flex>
--- src/components/fireconfig.js
+++ src/components/fireconfig.js
@@ -1,5 +1,6 @@
-import firebase from 'firebase/app';
-import 'firebase/auth';
+import firebase from 'firebase/compat/app';
+import "firebase/compat/auth";
+
 const firebaseConfig = {
     apiKey: "AIzaSyAgAulbkyPVrua3MLe_sS9T59buEp2mAwU",
     authDomain: "prekiassesment.firebaseapp.com",
@@ -10,5 +11,6 @@ const firebaseConfig = {
 };
  // Initialize Firebase
   const fire = firebase.initializeApp(firebaseConfig);
-  const auth = fire.auth()
-  export {auth}
+
+
+  export {fire}
